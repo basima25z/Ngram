89692670d74fd26e271afc8ea75e02eef3420a44
Basima Zafar <basimazafar@Basimas-MacBook-Pro.local>
2021-02-28 15:47:19 -0500

    finished NGRAM calc, TODO: Frequency and prediction

--- ngram.py
+++ ngram.py
@@ -1,6 +1,9 @@
 
 import sys
 import re
+from nltk.tokenize import sent_tokenize
+import nltk
+nltk.download('punkt')
 
 def punctuation_removal(fileContents):
     punc='''()-[];:'"\,/@#$%^&*_~'''
@@ -17,19 +20,59 @@ def punctuation_removal(fileContents):
 def ngram_calc(fileContents, numGrams):
     #first you have to remove punctuation
     fileContents=punctuation_removal(fileContents)
-    print(fileContents)
+    # print(fileContents)
     split_on_periods = re.split("\.", fileContents)
     print(split_on_periods)
-    sequences=[]
+    # sequences=[]
+
+    token_text =[]
+    token_text = sent_tokenize(fileContents)
+    print(token_text)
+
+    parts=[]
+    parts = fileContents.split(".")
+    print(parts)
+
 
     #sequences = ' '.join('<start> {} </start>'.format(l.strip()) for l in fileContents.split() if len(l.strip()))
     #print(sequences)
+#sequences.append([tags.split()[i:] for i in range(numGrams)])
 
-    for tags in split_on_periods:
-        tags = "<start>" + tags + "<end>"
-        sequences.append([tags.split()[i:] for i in range(numGrams)])
-    
-    print(sequences)
+    #adds start and end tags per sentance
+    sequences=[]
+    for tags in parts:
+        tags = "<start> " * (numGrams-1) + tags + " <end>"
+        #sequences.append(tags)
+        sequences.append(tags)
+    print("Sequences: ", sequences)
+
+
+
+    #now need to split each tag into ngrams
+    spaced=[]
+    finalSpaced=[]
+    ngrams=[]
+    for i in sequences:
+        print("I: ", i)
+        #spaced = re.split("\\s+", i)
+        spaced = i.split()
+        
+        print("spaced word: ", spaced)
+        finalSpaced.append(spaced)
+
+        for i in range(len(spaced)-numGrams+1):
+            temp=[spaced[j] for j in range(i,i+numGrams)]
+            ngrams.append(" ".join(temp))
+    print("Spaced:", finalSpaced)
+    print("ngrams: :", ngrams)
+
+    # ngrams=[]
+    # for i in finalSpaced:
+    #     for i in range(len(finalSpaced)-numGrams+1):
+    #         temp=[finalSpaced[j] for j in range(i,i+numGrams)]
+    #         #ngrams.append(" ".join(temp))
+    #         ngrams.append(" ".join(map(str, temp)))
+    #     print("ngrams: ", ngrams)
 
 
     # periodSplit = fileContents.split("\.")
